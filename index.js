29413625948f5c5ed2fd9c9ea575105419aa1de3
Adil Zhexenov <adilzhexenoff@gmail.com>
2025-03-05 20:34:48 +0500

    lesson 14: mongodb driver case

--- index.js
+++ index.js
@@ -1,14 +1,75 @@
 import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
+const connectionString = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(connectionString);
 
 app.use(express.json());
+
+let db;
+
+async function connectDatabase() {
+  try {
+    await client.connect();
+    db = client.db("lesson");
+    console.log("База данных успешно подключена!");
+  } catch (error) {
+    console.log("Не удалось подключиться к базе данных!");
+  }
+}
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
+app.get("/employees", async (req, res) => {
+  try {
+    const collection = db.collection("employees");
+    const employees = await collection.find().toArray();
+    res.json(employees);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/employees/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const collection = db.collection("employees");
+    const employee = await collection.findOne({
+      _id: new ObjectId(id)
+    });
+    res.json(employee);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/employees", async (req, res) => {
+  try {
+    const { fullName, salary, department } = req.body;
+
+    if (!fullName || !salary || !department) {
+      return res.status(400).json({ error: "Заполните все поля" });
+    }
+
+    const collection = db.collection("employees");
+
+    const employee = await collection.insertOne({
+      fullName,
+      salary,
+      department
+    });
+    res.json({ message: "Новый работчик был успешно создан", employee });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, async () => {
+  await connectDatabase();
   console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
